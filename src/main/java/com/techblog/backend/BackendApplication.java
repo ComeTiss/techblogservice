d048130097f021deed881ee6b97427ef440069d5
Come Tisserand <come_tisserand@Comes-MacBook-Pro.local>
2020-02-03 21:58:44 +0100

    hello world running

--- src/main/java/com/techblog/backend/BackendApplication.java
+++ src/main/java/com/techblog/backend/BackendApplication.java
@@ -1,13 +1,21 @@
 package com.techblog.backend;
 
+import lombok.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@RequestMapping(value = "/")
+	public String name() {
+		return "Hello World";
+	}
 }
