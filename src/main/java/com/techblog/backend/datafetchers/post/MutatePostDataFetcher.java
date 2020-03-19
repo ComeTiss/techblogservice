deb3fb7e99b35c4b144c43912b968b4687d5103b
ComeTiss <cometisserand@rocketmail.com>
2020-03-19 18:08:02 +0000

    store credentials

--- src/main/java/com/techblog/backend/datafetchers/post/MutatePostDataFetcher.java
+++ src/main/java/com/techblog/backend/datafetchers/post/MutatePostDataFetcher.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
 @Component
 @Slf4j
 public class MutatePostDataFetcher implements DataFetcher<Post> {
