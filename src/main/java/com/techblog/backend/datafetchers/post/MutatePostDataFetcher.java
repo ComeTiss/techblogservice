f5b123ab91874fa4cc042d1c383f443cdf9592e6
ComeTiss <cometisserand@rocketmail.com>
2020-03-19 16:23:28 +0000

    test save git credentials

--- src/main/java/com/techblog/backend/datafetchers/post/MutatePostDataFetcher.java
+++ src/main/java/com/techblog/backend/datafetchers/post/MutatePostDataFetcher.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+
 @Component
 @Slf4j
 public class MutatePostDataFetcher implements DataFetcher<Post> {
