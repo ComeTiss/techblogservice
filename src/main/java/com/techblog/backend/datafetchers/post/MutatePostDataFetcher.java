6bb21b2116c40f9613ab526a64f5445ba9dbe4a1
ComeTiss <cometisserand@rocketmail.com>
2020-03-21 10:26:36 +0000

    [graphQL] added input object to graphql schema for create post

--- src/main/java/com/techblog/backend/datafetchers/post/MutatePostDataFetcher.java
+++ src/main/java/com/techblog/backend/datafetchers/post/MutatePostDataFetcher.java
@@ -5,6 +5,7 @@ import com.techblog.backend.repository.PostRepository;
 import graphql.GraphQLException;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
+import java.util.LinkedHashMap;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -23,16 +24,16 @@ public class MutatePostDataFetcher implements DataFetcher<Post> {
    */
   @Override
   public Post get(DataFetchingEnvironment environment) {
-    String postId = environment.getArgument("id");
-    String title = environment.getArgument("title");
-    String description = environment.getArgument("description");
+    LinkedHashMap postDataMap = environment.getArgument("post");
+    String title = postDataMap.get("title").toString();
+    String description = postDataMap.get("description").toString();
 
     if (title.isEmpty() || description.isEmpty()) {
       throw new GraphQLException("Post title/description cannot be empty");
     }
     try {
-      if (postId != null) {
-        Post currentPost = postRepository.getOne(Long.valueOf(postId));
+      if (postDataMap.get("id") != null) {
+        Post currentPost = postRepository.getOne(Long.valueOf(postDataMap.get("id").toString()));
         currentPost.setDescription(description);
         currentPost.setTitle(title);
         return postRepository.save(currentPost);
