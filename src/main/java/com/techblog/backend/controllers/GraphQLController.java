60cf27a079c0e5b041d79149e35e374fce75dfac
ComeTiss <cometisserand@rocketmail.com>
2020-03-28 11:34:21 +0000

    [mino] fix CI with spotles apply

--- src/main/java/com/techblog/backend/controllers/GraphQLController.java
+++ src/main/java/com/techblog/backend/controllers/GraphQLController.java
@@ -4,19 +4,15 @@ import com.techblog.backend.GraphQLService;
 import com.techblog.backend.types.BaseRequestData;
 import com.techblog.backend.types.BaseResponse;
 import com.techblog.backend.types.error.ServiceError;
-import com.techblog.backend.types.error.ServiceExceptionHandler;
 import com.techblog.backend.types.error.ServiceErrorMessage;
 import com.techblog.backend.types.error.ServiceExceptionType;
 import com.techblog.backend.types.post.BaseResponseData;
 import graphql.ExecutionInput;
 import graphql.ExecutionResult;
-import graphql.GraphQLError;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @CrossOrigin
 @RestController()
@@ -44,8 +40,7 @@ public class GraphQLController {
         return new BaseResponse(serviceError, HttpStatus.BAD_REQUEST);
       }
       return new BaseResponse(new BaseResponseData(executionResult.getData()), HttpStatus.OK);
-    }
-    catch (Exception e) {
+    } catch (Exception e) {
       log.error(e.getMessage());
       return new BaseResponse(
           new ServiceErrorMessage("An error occurred"), HttpStatus.INTERNAL_SERVER_ERROR);
